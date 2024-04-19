9f9866555a0a36d584f9a5522ba7b7ace6f61ecc
yatender saini <yatender.saini@gmail.com>
2024-04-19 06:37:23 +0530

    change in app js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world to yatender saini.
+          Hello to the new world yatender saini.
         </p>
         <a
           className="App-link"
